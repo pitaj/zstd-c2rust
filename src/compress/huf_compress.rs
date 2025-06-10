6d9294b45cb00bbdcea128f0e6e9e0a8a67da81f
Peter Jaszkowiak <p.jaszkow@gmail.com>
2025-06-09 20:58:07 -0600

    const

--- src/compress/huf_compress.rs
+++ src/compress/huf_compress.rs
@@ -276,11 +276,11 @@ unsafe extern "C" fn ERR_isError(mut code: size_t) -> std::ffi::c_uint {
         as std::ffi::c_uint;
 }
 #[inline]
-unsafe extern "C" fn ZSTD_countLeadingZeros32(mut val: U32) -> std::ffi::c_uint {
+const extern "C" fn ZSTD_countLeadingZeros32(mut val: U32) -> std::ffi::c_uint {
     return val.leading_zeros() as i32 as std::ffi::c_uint;
 }
 #[inline]
-unsafe extern "C" fn ZSTD_highbit32(mut val: U32) -> std::ffi::c_uint {
+const extern "C" fn ZSTD_highbit32(mut val: U32) -> std::ffi::c_uint {
     return (31 as std::ffi::c_int as std::ffi::c_uint)
         .wrapping_sub(ZSTD_countLeadingZeros32(val));
 }
